0a69c5634c9cf7c50077ad7b230c205aeaf8afab
levi davis <levidavis@pursuit.org>
2019-08-13 20:02:32 -0400

    Removes commented out lines

--- AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/ZooAnimalTableViewController.swift
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/ZooAnimalTableViewController.swift
@@ -21,17 +21,11 @@ class ZooAnimalTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
+  
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 5
     }
     
@@ -55,7 +49,6 @@ class ZooAnimalTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         switch section {
         case 0:
             return mammals.count
@@ -75,13 +68,6 @@ class ZooAnimalTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let oneAnimal = zooAnimal[indexPath.row]
-//        let mammal = mammals[indexPath.row]
-//        let reptile = reptiles[indexPath.row]
-//        let bird = birds[indexPath.row]
-//        let insect = insects[indexPath.row]
-//        let amphibian = amphibians[indexPath.row]
-//
         switch indexPath.section {
         case 0:
             if let cell = tableView.dequeueReusableCell(withIdentifier: "CustomZooCell", for: indexPath) as? ZooAnimalTableViewCell {
@@ -136,7 +122,6 @@ class ZooAnimalTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //we want to move to the vc with the storyboard ID "movieDetailViewController"
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
         if let viewController = storyBoard.instantiateViewController(withIdentifier: "ZooAnimalDetailViewController") as? ZooAnimalDetailViewController {
             switch indexPath.section {
@@ -161,67 +146,5 @@ class ZooAnimalTableViewController: UITableViewController {
 
         }
     }
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        guard let segueIdentifier = segue.identifier else {fatalError("no id in segue")}
-//
-//        switch segueIdentifier {
-//        case "ZooDetailSegue":
-//            guard let zooAnimalDetailVC = segue.destination as? ZooAnimalDetailViewController else {
-//                fatalError("oof")
-//            }
-//            guard let selectedIndexPath = tableView.indexPathForSelectedRow else {
-//                fatalError("ufda")
-//            }
-//            zooAnimalDetailVC.zooAnimal = zooAnimal[selectedIndexPath.row]
-//        default:
-//            fatalError("double oof")
-//        }
-//    }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
