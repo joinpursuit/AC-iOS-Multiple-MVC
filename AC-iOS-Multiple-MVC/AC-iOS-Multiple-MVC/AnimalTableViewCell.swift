a1e1bf468a54a9043562bbeca86539dc4b7942bb
Pursuit <pursuit@pursuits-mbp.home>
2019-08-13 22:42:01 -0400

    Answers added for MVC Lab

---
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/AnimalTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  AnimalTableViewCell.swift
+//  AC-iOS-Multiple-MVC
+//
+//  Created by Pursuit on 8/13/19.
+//  Copyright © 2019 AC-iOS. All rights reserved.
+//
+
+import UIKit
+
+class AnimalTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var animalImage: UIImageView!
+    
+    @IBOutlet weak var animalLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+   
+
+}
