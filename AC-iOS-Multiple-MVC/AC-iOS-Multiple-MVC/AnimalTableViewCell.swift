a4d1858a3dd84aa6fe7e20863ca652a88cbe06fa
Ahad Islam <34593133+ahadislam1@users.noreply.github.com>
2019-12-15 02:51:43 -0500

    finished lab

---
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/AnimalTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalTableViewCell.swift
+//  AC-iOS-Multiple-MVC
+//
+//  Created by Ahad Islam on 12/15/19.
+//  Copyright © 2019 AC-iOS. All rights reserved.
+//
+
+import UIKit
+
+class AnimalTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var animalImageView: UIImageView!
+    @IBOutlet weak var animalTitleLabel: UILabel!
+    @IBOutlet weak var animalLocationLabel: UILabel!
+    
+}
--- AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/ViewController.swift
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/ViewController.swift
@@ -11,16 +11,69 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let animals = ZooAnimal.zooAnimals
+    
+    private var groupedAnimalsKey: [String] {
+        Dictionary(grouping: animals, by: {$0.classification})
+            .sorted(by: {$0.key < $1.key})
+            .map({$0.key})
+    }
+    
+    private var groupedAnimals: [[ZooAnimal]] {
+        Dictionary(grouping: animals, by: {$0.classification})
+            .sorted(by: {$0.key < $1.key})
+            .map {$0.value}
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destVC = segue.destination as? DetailViewController {
+            destVC.animal = groupedAnimals[tableView.indexPathForSelectedRow!.section][tableView.indexPathForSelectedRow!.row]
+        }
+    }
+    
+    private func configureView() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate {}
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return groupedAnimalsKey[section]
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return groupedAnimals.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groupedAnimals[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Animal Cell", for: indexPath) as? AnimalTableViewCell else {
+            print("Error occured creating cell.")
+            return UITableViewCell()
+        }
+        let animal = groupedAnimals[indexPath.section][indexPath.row]
+        cell.animalTitleLabel.text = animal.name
+        cell.animalLocationLabel.text = animal.origin
+        cell.animalImageView.image = UIImage(named: String(animal.imageNumber))
+        return cell
+        
+    }
+}
